f829fae931b7843bbb978bd125548e357b61d009
Eduardo Fernandes <edu.fernandes.pt@gmail.com>
2019-03-04 12:47:10 +0000

    Add professions fixtures

---
+++ src/DataFixtures/LoadProfessions.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\DataFixtures\AbstractDataFixture;
+use App\Entity\Profession;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadProfessions extends AbstractDataFixture
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getOrder()
+    {
+        return 3;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $this->loadData('professions.yaml');
+        foreach (array_keys($this->getData()) as $key) {
+            $item = $this->getData($key);
+            $profession = new Profession();
+            $profession->setName($key);
+            $manager->persist($profession);
+            $manager->flush();
+            $this->setReference('profession_'.$this->slugify($key), $manager);
+            $this->stepIt();
+        }
+        $this->getOutput()->writeln('');
+    }
+}
