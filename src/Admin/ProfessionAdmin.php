4ee65f172a8d2b1b207f3058c3c272b50cc6041a
Eduardo Fernandes <edu.fernandes.pt@gmail.com>
2019-03-05 12:56:08 +0000

    Implement ProfessionAdmin

---
+++ src/Admin/ProfessionAdmin.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+use App\Admin\Type\MarkDownType;
+
+final class ProfessionAdmin extends AbstractAdmin
+{
+    protected $baseRouteName = 'shared_profession';
+    protected $baseRoutePattern = 'shared/profession';
+    protected $datagridValues = [
+        '_sort_by' => 'name',
+        '_sort_order' => 'ASC',
+        '_per_page' => 512,
+    ];
+    protected $maxPerPage = 512;
+    protected $perPageOptions = [64, 128, 256, 512];
+    protected $translationDomain = 'profession';
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper): void
+    {
+        $datagridMapper
+            ->add('id', null, ['label' => 'admin.label.id'])
+            ->add('name', null, ['label' => 'admin.label.name']);
+    }
+
+    protected function configureListFields(ListMapper $listMapper): void
+    {
+        $listMapper
+            ->add('id', null, ['label' => 'admin.label.id'])
+            ->add('name', null, ['label' => 'admin.label.name'])
+            ->add(
+                'description',
+                null,
+                [
+                    'label' => 'admin.label.description',
+                    'template' => 'form/list/markdown.html.twig',
+                ]
+            )
+            ->add('_action', null, [
+                'actions' => [
+                    'show' => [],
+                    'edit' => [],
+                    'delete' => [],
+                ],
+            ]);
+    }
+
+    protected function configureFormFields(FormMapper $formMapper): void
+    {
+        $formMapper
+            ->add('name', null, ['label' => 'admin.label.name'])
+            ->add('description', MarkDownType::class, ['label' => 'admin.label.description']);
+    }
+
+    protected function configureShowFields(ShowMapper $showMapper): void
+    {
+        $showMapper
+            ->add('id', null, ['label' => 'admin.label.id'])
+            ->add('name', null, ['label' => 'admin.label.name'])
+            ->add(
+                'description',
+                null,
+                [
+                    'label' => 'admin.label.description',
+                    'template' => 'form/show/markdown.html.twig',
+                ]
+            );
+    }
+}
