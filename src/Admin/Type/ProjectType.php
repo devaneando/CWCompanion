041cc58b6a09885d4516c64a00ac80726b62d4ea
Eduardo Fernandes <edu.fernandes.pt@gmail.com>
2019-04-03 18:05:43 +0100

    Implement ProjectType

---
+++ src/Admin/Type/ProjectType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Admin\Type;
+
+use App\Entity\Project;
+use App\Traits\LoggedUserTrait;
+use App\Traits\Repository\ProjectRepositoryTrait;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProjectType extends ChoiceType
+{
+    use LoggedUserTrait;
+    use ProjectRepositoryTrait;
+
+    protected function getProjects(): array
+    {
+        return $this->getProjectRepository()->getProjectArrayByOwner($this->getLoggedUser());
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'choices' => $this->getProjects(),
+            'choices_as_values' => true,
+            'choice_translation_domain' => false,
+            'multiple' => false,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'project';
+    }
+
+    public function getParent()
+    {
+        return ChoiceType::class;
+    }
+}
--- src/Entity/Repository/ProjectRepository.php
+++ src/Entity/Repository/ProjectRepository.php
@@ -5,6 +5,7 @@ namespace App\Entity\Repository;
 use App\Entity\Project;
 use App\Entity\Repository\AbstractBaseRepository;
 use App\Entity\User;
+use Doctrine\DBAL\FetchMode;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
 class ProjectRepository extends AbstractBaseRepository
@@ -12,5 +13,26 @@ class ProjectRepository extends AbstractBaseRepository
     public function __construct(RegistryInterface $registry)
     {
         parent::__construct($registry, Project::class);
+    }
+
+    public function getProjectArrayByOwner(User $owner): array
+    {
+        $sql = 'SELECT name, id FROM projects';
+        $params = [];
+        if (true === $owner->isSuperAdmin()) {
+            $sql .= ' WHERE owner_id = :owner';
+            $params['owner'] = $owner->getId();
+        }
+        $sql .= ' ORDER BY name ASC';
+
+        $statement = $this->getEntityManager()->getConnection()->prepare($sql);
+        $statement->execute($params);
+
+        $result = [];
+        foreach ($statement->fetchAll(FetchMode::ASSOCIATIVE) as $item) {
+            $result[$item['name']] = $item['id'];
+        }
+
+        return $result;
     }
 }
